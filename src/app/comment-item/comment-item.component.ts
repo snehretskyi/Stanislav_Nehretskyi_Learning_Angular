c49814282271972b71f5a3c88dc8846fd0bc4d5f
Stanislav Nehretskyi <w0859305@myscc.ca>
2024-09-24 10:10:35 -0400

    Generate components

---
+++ src/app/comment-item/comment-item.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-comment-item',
+  standalone: true,
+  imports: [],
+  templateUrl: './comment-item.component.html',
+  styleUrl: './comment-item.component.css'
+})
+export class CommentItemComponent {
+
+}
