5ccfcf62c37d7ecfaec23c743f1e88b5350d714f
“Stanislav <w0859305@myscc.ca>
2024-10-08 12:27:27 -0400

    Finish Bonus Assignment 5

--- src/app/comment-item/comment-item.component.ts
+++ src/app/comment-item/comment-item.component.ts
@@ -1,17 +1,33 @@
 import {Component, Input} from '@angular/core';
 import {Comment} from "../models/comment";
 import {NgIf, NgOptimizedImage} from "@angular/common";
+import {ActivatedRoute, RouterLinkActive} from "@angular/router";
+import {CommentService} from "../services/comment.service";
 
 @Component({
   selector: 'app-comment-item',
   standalone: true,
   imports: [
     NgIf,
-    NgOptimizedImage
+    NgOptimizedImage,
+    RouterLinkActive
   ],
   templateUrl: './comment-item.component.html',
   styleUrl: './comment-item.component.css'
 })
 export class CommentItemComponent {
-  @Input() comment?: Comment;
+  @Input() comment: Comment | undefined;
+
+  constructor(private route: ActivatedRoute,
+              private commentService: CommentService) {
+  }
+
+  ngOnInit() {
+    const id = this.route.snapshot.paramMap.get('id');
+    if(id){
+      this.commentService.getCommentById(Number(id)).subscribe(comment =>{
+        this.comment = comment;
+      })
+    }
+  }
 }
