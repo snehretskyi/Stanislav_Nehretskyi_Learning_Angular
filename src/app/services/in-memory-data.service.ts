9bddb2e39af2bebd4456445f1a8f9f8a77e53bc9
“Stanislav <w0859305@myscc.ca>
2024-11-06 09:09:48 -0500

    Finish In Memory Data Service

---
+++ src/app/services/in-memory-data.service.ts
@@ -0,0 +1,43 @@
+import { Injectable } from '@angular/core';
+import {InMemoryDbService} from "angular-in-memory-web-api";
+import {CommentAuthor} from "../models/comment-author";
+import {Comment} from "../models/comment";
+import {Observable} from "rxjs";
+
+class RequestInfo_2 {
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class InMemoryDataService implements InMemoryDbService {
+  createDb(reqInfo?: RequestInfo_2): {} | Observable<{}> | Promise<{}> {
+    const personList: CommentAuthor[] = //any[] would have worked as well
+      [{id: 1, firstName: "John", lastName:"Doe", nickname: "Johnathan",
+        profileImageURL: "assets/800px-La_Promenade,_by_Pierre-Auguste_Renoir.jpg",
+        emailAddress:"johndoe@yahoo.net"},
+        {id: 2,firstName: "Jane", lastName:"Doe", nickname: "D0m1nat0r",
+          profileImageURL: "assets/Olivia_de_Havilland,_actress,_1985_-_levels_adjustment.jpg",
+          emailAddress:"pdoe123@gmail.com"},
+        {id: 3,firstName: "Patrick", lastName:"Star", nickname: "Patrick",
+          profileImageURL: "assets/Patrick_Star.svg.webp",
+          phoneNumber: "+12345678910", emailAddress:"pstar@gmail.com"},
+        {id: 4,firstName: "Man", lastName:"In Yellow Hat", nickname: "Man in the yellow hat",
+          profileImageURL: "assets/Curious_George.png",
+          emailAddress:"yellowhat@zoo.com"},
+        {id: 5,firstName: "Grzegorz", lastName:"Wisniewski", nickname: "Cherry5431",
+          profileImageURL: "assets/Cherry_season_(48216568227).jpg",
+          emailAddress:"nirvana@gmail.com"},
+        {id: 6, firstName: "Alexander", lastName:"Bell", nickname: "Alexander Graham Bell",
+          profileImageURL:"assets/Alexander_Graham_Bell.jpg",
+          phoneNumber: "+1", emailAddress:"alexanderbell@arpa.net"}];
+    const commentArray:Comment[] = [{id: 1, author: personList[0], body:"Hi", likes:0},
+      {id: 2, author: personList[1], body:"Howdy", likes:0},
+      {id: 3, author: personList[2], body:"Hello", likes:0},
+      {id: 4, author: personList[3], body:"I like George", likes:3},
+      {id: 5, author: personList[4], body:"Hello World", likes:0},
+      {id: 6, author: personList[5], body:"Greetings Gentlemen", likes:0}];
+    return {commentArray, personList};
+  }
+
+}
