8ffd7a831caf05b09bd7bff85421f504e181574a
“Stanislav <w0859305@myscc.ca>
2024-11-13 10:22:09 -0500

    Finish Assignment 8

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,9 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { provideRouter, Routes } from '@angular/router';
 import { appConfig } from './app/app.config';
+import {PreloadAllModules} from "@angular/router";
 import { AppComponent } from './app/app.component';
-import {CommentItemComponent} from "./app/comment-item/comment-item.component";
 import {CommentsListComponent} from "./app/comments-list/comments-list.component";
-import {ModifyCommentComponent} from "./app/modify-comment/modify-comment.component";
-import {PageNotFoundComponent} from "./app/page-not-found/page-not-found.component";
 import {provideHttpClient} from "@angular/common/http";
 import {importProvidersFrom} from "@angular/core";
 import {HttpClientInMemoryWebApiModule} from "angular-in-memory-web-api";
@@ -14,9 +12,9 @@ import {InMemoryDataService} from "./app/services/in-memory-data.service";
 const routes: Routes = [
   {path:'', redirectTo: '/comments', pathMatch: 'full'}, //default route
   { path: 'comments', component: CommentsListComponent },
-  { path: 'modify-comment/:id', component: ModifyCommentComponent },
-  {path:'modify-comment', component: ModifyCommentComponent},
-  {path: '**', component:PageNotFoundComponent}//Wildcard route for a 404 page
+  { path: 'modify-comment/:id', loadComponent: () => import('./app/modify-comment/modify-comment.component').then(m => m.ModifyCommentComponent) },
+  {path:'modify-comment', loadComponent: () => import('./app/modify-comment/modify-comment.component').then(m => m.ModifyCommentComponent)},
+  {path: '**', loadComponent:() => import('./app/page-not-found/page-not-found.component').then(m => m.PageNotFoundComponent)}//Wildcard route for a 404 page
 ];
 
 // I changed delay because 1000ms is too slow
