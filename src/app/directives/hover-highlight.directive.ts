fadb97d53a357466d17ecaa9e29d4c81272f9fc0
Stanislav Nehretskyi <w0859305@myscc.ca>
2024-11-19 10:04:45 -0500

    Start Assignment 9

---
+++ src/app/directives/hover-highlight.directive.ts
@@ -0,0 +1,23 @@
+import {Directive, ElementRef, HostListener, Input} from '@angular/core';
+
+@Directive({
+  selector: '[appHoverHighlight]',
+  standalone: true
+})
+export class HoverHighlightDirective {
+  @Input() hoverHighlight = '';
+
+  constructor(private el: ElementRef) { }
+
+  @HostListener('mouseenter') onMouseEnter() {
+    this.highlight(this.hoverHighlight || 'blue');
+  }
+  @HostListener('mouseleave') onMouseLeave() {
+    this.highlight('');
+  }
+
+  private highlight(color: string) {
+    this.el.nativeElement.style.backgroundColor = color;
+  }
+
+}
